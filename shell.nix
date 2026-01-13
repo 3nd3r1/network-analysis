56f3a2cb0e8ad70c101646739e393e94da4242a9
3nd3r1 <ender.spam.guru@gmail.com>
2026-01-13 16:58:57 +0200

    chore: init the repo

---
+++ shell.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    python3
+    python3Packages.matplotlib
+    python3Packages.numpy
+    python3Packages.networkx
+  ];
+}
