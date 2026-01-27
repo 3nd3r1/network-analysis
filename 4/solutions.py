5bc5b201d29a4e0913db4cb88921444f30b8dbbc
3nd3r1 <ender.spam.guru@gmail.com>
2026-01-27 16:51:04 +0200

    4: implement the balance_test

--- 4/solutions.py
+++ 4/solutions.py
@@ -1,6 +1,42 @@
 import networkx as nx
 
 
+def balance_test(graph: nx.Graph) -> bool:
+    graph_plus = nx.Graph()
+    for node in graph.nodes():
+        graph_plus.add_node(node)
+    for edge in graph.edges(data=True):
+        if edge[2]["sign"] == "+":
+            graph_plus.add_edge(edge[0], edge[1])
+
+    components = nx.connected_components(graph_plus)
+    graph_supernodes = nx.Graph()
+    node_component = {}
+    for i, component in enumerate(components):
+        graph_supernodes.add_node(i)
+        for node in component:
+            node_component[node] = i
+            for edge in graph.edges(node, data=True):
+                if edge[1] in component and edge[2]["sign"] == "-":
+                    return False
+
+    for node, i in node_component.items():
+        for edge in graph.edges(node):
+            if node_component[edge[1]] != i and not graph_supernodes.has_edge(
+                i, node_component[edge[1]]
+            ):
+                graph_supernodes.add_edge(i, node_component[edge[1]])
+
+    for node in graph_supernodes.nodes():
+        for layer in nx.bfs_layers(graph_supernodes, node):
+            for i in range(0, len(layer)):
+                for j in range(i + 1, len(layer)):
+                    if graph_supernodes.has_edge(layer[i], layer[j]):
+                        return False
+
+    return True
+
+
 graph_n1 = nx.read_edgelist(
     "graph_n1.edgelist",
     nodetype=int,
@@ -11,3 +47,6 @@ graph_n2 = nx.read_edgelist(
     nodetype=int,
     data=[("sign", str)],  # type: ignore
 )
+
+print(balance_test(graph_n1))
+print(balance_test(graph_n2))
